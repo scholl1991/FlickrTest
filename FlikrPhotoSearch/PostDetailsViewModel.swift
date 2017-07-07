4dddead616b472ecdc8dc9fedee12682689ece46
Sergii Shulga <sergii.shulga@altexsoft.com>
2017-07-08 02:01:49 +0300

    gallery

---
+++ FlikrPhotoSearch/PostDetailsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostDetailsViewModel.swift
+//  FlikrPhotoSearch
+//
+//  Created by Sergii Shulga on 7/7/17.
+//  Copyright © 2017 Sergii Shulga. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailsViewModel: NSObject {
+
+}
